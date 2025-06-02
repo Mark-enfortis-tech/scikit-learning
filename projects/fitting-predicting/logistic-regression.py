3a33a40cb8d6a8729c9b5194b586acc4bda3eb1b
Mark Saunders <enfortis.tech@gmail.com>
2025-06-02 06:19:43 -0500

    v0.0.4

--- projects/fitting-predicting/logistic-regression.py
+++ projects/fitting-predicting/logistic-regression.py
@@ -10,8 +10,24 @@ iris = load_iris()
 X = iris.data
 y = iris.target
 
+print('X:',X)
+num_rows = X.shape[0]
+print(f"Number of rows: {num_rows}")
+num_cols = X.shape[1]
+print(f"Number of cols: {num_cols}")
+
+print('y:', y)
+
 # split into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+
+print('X_train:', X_train)
+num_rows = X_train.shape[0]
+print(f"Number of rows: {num_rows}")
+
+print('X_test:', X_test)
+num_rows = X_test.shape[0]
+print(f"Number of rows: {num_rows}")
 
 # define model
 model = LogisticRegression(max_iter=200)
@@ -21,6 +37,7 @@ model.fit(X_train, y_train)
 
 # predict on test data
 y_pred = model.predict(X_test)
+print('y_pred: ', y_pred)
 
 # evaluate accuracy
 accuracy = accuracy_score(y_test, y_pred)
